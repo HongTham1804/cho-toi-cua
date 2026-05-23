3fc48a3bd4e793ecaf065e3d93cee3fadff18b10
Thanh Thuy <thachthanhthuy287@gmail.com>
2026-05-23 23:47:13 +0700

    Feat: Cập nhật giao diện đăng nhập và quản lí sản phẩm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,12 +28,12 @@ function App() {
   )
 =======
 ﻿import React from 'react';
-import PartnerLogin from './pages/PartnerLogin/PartnerLogin'; // Đảm bảo đúng đường dẫn đến file component của bạn
+import ProductManagement from "./pages/ProductManagement/ProductManagement"; // Đảm bảo đường dẫn này đúng với nơi bạn lưu file
 
 function App() {
   return (
     <div className="App">
-      <PartnerLogin />
+      <ProductManagement />
     </div>
   );
 >>>>>>> 0215432e6007e1eb9e7008acdb46e310ea7f0a87
