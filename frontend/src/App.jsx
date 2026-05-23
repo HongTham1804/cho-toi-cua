ec6e0422d9d134b74553748bae0b12af92dbc9ab
Thanh Thuy <thachthanhthuy287@gmail.com>
2026-05-23 23:51:53 +0700

    Cập nhật các thay đổi cuối cùng và fix file lock

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,4 @@
-<<<<<<< HEAD
-import { Navigate, Route, Routes } from 'react-router-dom'
-import './App.css'
-import RegisterStore from './pages/register-store'
-import Login from './pages/login'
-import Notifications from './pages/notifications'
-import OrderHistory from './pages/order-history'
-import Review from './pages/review'
-import Tracking from './pages/tracking'
-import OrderManagement from './pages/order-management'
-import Inventory from './pages/inventory'
-import ProductDetail from './pages/product-detail'
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/register-store" element={<RegisterStore />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/notifications" element={<Notifications />} />
-      <Route path="/order-history" element={<OrderHistory />} />
-      <Route path="/review" element={<Review />} />
-      <Route path="/tracking" element={<Tracking />} />
-      <Route path="/order-management" element={<OrderManagement />} />
-      <Route path="/inventory" element={<Inventory />} />
-      <Route path="/product/:id" element={<ProductDetail />} />
-    </Routes>
-  )
-=======
 ﻿import React from 'react';
 import ProductManagement from "./pages/ProductManagement/ProductManagement"; // Đảm bảo đường dẫn này đúng với nơi bạn lưu file
 
